9e5cbfb8a709fd4765935e1d15f98ff99a42e8f5
Olesya <olesyabrazhnikova@ukr.net>
2022-07-04 16:37:11 +0300

    added auth user

---
+++ resources/admin/js/modules/auth.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+let state = {
+    _user: {}
+}
+
+let getters = {
+    GET_USER (state){
+        return state._user;
+    }
+}
+
+let mutations = {
+    SET_USER (state, userData) {
+        state._user = userData;
+    }
+}
+
+let actions = {
+    async LOAD_USER ({commit}) {        
+        try {
+            const res = await axios({
+                method: "post",
+                url: "/api/v2/auth"
+            })
+            console.log(res);
+            commit("SET_USER", res.data);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+export default {
+    state,
+    getters,
+    mutations,
+    actions,
+};
