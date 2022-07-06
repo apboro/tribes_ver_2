e5416844a4f0789411524d3b0750e5a35ddec59a
Pyatakov Oleg <pyatak.ov@yandex.ru>
2022-07-06 17:47:36 +0300

    add multiple operations styles

--- resources/js/components/Knowledge/components/Knowledge/KnowledgeMultipleOperations.vue
+++ resources/js/components/Knowledge/components/Knowledge/KnowledgeMultipleOperations.vue
@@ -1,35 +1,35 @@
 <template>
     <div class="knowledge__multiple-operations">
         <button
-            class="knowledge__multiple-btn"
+            class="button-text button-text--danger knowledge__multiple-btn"
             @click="setOperationType('delete')"
         >
             Удалить
         </button>
         
         <button
-            class="knowledge__multiple-btn"
+            class="button-text button-text--primary knowledge__multiple-btn"
+            @click="setOperationType('no_public')"
+        >
+            Снять с публикации
+        </button>
+
+        <button
+            class="button-text button-text--primary knowledge__multiple-btn"
             @click="setOperationType('public')"
         >
             Опубликовать
         </button>
         
         <button
-            class="knowledge__multiple-btn"
-            @click="setOperationType('no_public')"
-        >
-            Снять с публикации
-        </button>
-        
-        <button
-            class="knowledge__multiple-btn"
+            class="button-text button-text--primary knowledge__multiple-btn"
             @click="setOperationType('draft')"
         >
-            Черновик
+            В черновик
         </button>
         
         <button
-            class="knowledge__multiple-btn"
+            class="button-text button-text--primary knowledge__multiple-btn"
             @click="setOperationType('no_draft')"
         >
             Не черновик
