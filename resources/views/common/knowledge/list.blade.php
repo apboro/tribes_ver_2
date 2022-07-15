30f0eceee2500847552c4bc4250d56e732ddad92
Pyatakov Oleg <pyatak.ov@yandex.ru>
2022-07-15 10:11:27 +0300

    add styles for knowledge public page

--- resources/views/common/knowledge/list.blade.php
+++ resources/views/common/knowledge/list.blade.php
@@ -82,9 +82,9 @@
                             <div class="knowledge-list__item-icon-wrapper">
                                 <i class="icon knowledge-list__item-icon icon--size-3">
                                     <svg width="100%" height="100%" viewBox="0 0 76 76" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path class="icon__stroke" fill-rule="evenodd" clip-rule="evenodd" d="M38 69.6668C55.489 69.6668 69.6666 55.4892 69.6666 38.0002C69.6666 20.5111 55.489 6.3335 38 6.3335C20.511 6.3335 6.33331 20.5111 6.33331 38.0002C6.33331 55.4892 20.511 69.6668 38 69.6668Z" fill="transparent" stroke="#7367F0" stroke-opacity="0.15" stroke-width="2"/>
-                                        <path class="icon__stroke" d="M38 46.3333C38 40 42.9907 38.9401 44.7092 37.2258C46.4333 35.506 47.5 33.1276 47.5 30.5C47.5 25.2533 43.2467 21 38 21C35.163 21 32.6164 22.2436 30.8756 24.2155C30.0707 25.1273 29.4382 26.1947 29.0288 27.3669" stroke="#7367F0" stroke-opacity="0.15" stroke-width="2" stroke-linecap="round"/>
-                                        <path class="icon__fill" fill-rule="evenodd" clip-rule="evenodd" d="M38 55C39.1046 55 40 54.1046 40 53C40 51.8954 39.1046 51 38 51C36.8954 51 36 51.8954 36 53C36 54.1046 36.8954 55 38 55Z" fill="#7367F0" fill-opacity="0.15"/>
+                                        <path class="icon__stroke" fill-rule="evenodd" clip-rule="evenodd" d="M38 69.6668C55.489 69.6668 69.6666 55.4892 69.6666 38.0002C69.6666 20.5111 55.489 6.3335 38 6.3335C20.511 6.3335 6.33331 20.5111 6.33331 38.0002C6.33331 55.4892 20.511 69.6668 38 69.6668Z" fill="transparent" stroke="#7367F0" stroke-width="2"/>
+                                        <path class="icon__stroke" d="M38 46.3333C38 40 42.9907 38.9401 44.7092 37.2258C46.4333 35.506 47.5 33.1276 47.5 30.5C47.5 25.2533 43.2467 21 38 21C35.163 21 32.6164 22.2436 30.8756 24.2155C30.0707 25.1273 29.4382 26.1947 29.0288 27.3669" stroke="#7367F0" stroke-width="2" stroke-linecap="round"/>
+                                        <path class="icon__fill" fill-rule="evenodd" clip-rule="evenodd" d="M38 55C39.1046 55 40 54.1046 40 53C40 51.8954 39.1046 51 38 51C36.8954 51 36 51.8954 36 53C36 54.1046 36.8954 55 38 55Z" fill="#7367F0" />
                                     </svg>
                                 </i>
 
