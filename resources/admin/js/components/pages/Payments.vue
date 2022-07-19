d7c78fbd3d6d5c861ff0045ba0af87f89e0c32b1
Olesya <olesyabrazhnikova@ukr.net>
2022-07-19 12:43:20 +0300

    output an array of objects payments and creating layout for it

--- resources/admin/js/components/pages/Payments.vue
+++ resources/admin/js/components/pages/Payments.vue
@@ -5,7 +5,7 @@
                 <div class="text-muted">
                     показать
                     <div class="mx-2 d-inline-block">
-                        <input type="text" class="form-control form-control-sm" size="3">
+                        <input type="text" class="form-control form-control-sm" v-model="filter_data.entries" size="3">
                         
                     </div>
                     на странице
@@ -24,66 +24,63 @@
                 <thead>
                 <tr>
                     <th class="w-1"><input class="form-check-input m-0 align-middle" type="checkbox" aria-label="Select all invoices"></th>
-                    <th>Search</th>
-                    <th>Date</th>
-                    <th>Sorting user</th>
-                    <th>Sorting date</th>
-                    <th>Entries</th>
-                    <th>Page</th>
+                    <th class="w-1">ID Платежа
+                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-sm text-dark icon-thick" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><polyline points="6 15 12 9 18 15"></polyline></svg>
+                    </th>
+                    <th>Дата платежа</th>
+                    <th>Пользователь</th>
+                    <th>Тип платежа</th>
+                    <th>Сообщество</th>
+                    <th>Статус</th>
+                    <th>Сумма</th>
                     <th></th>
                 </tr>
                 </thead>
                 <tbody>
 
-
-                <tr>
-                    <td><input class="form-check-input m-0 align-middle" type="checkbox" aria-label="Select invoice"></td>
-                    <td><span class="text-muted"></span></td>
-                    <td><span class="flag flag-country-pl">Date</span></td>
-                    <td>Sorting user</td>
-                    <td>Sorting date</td>
-                    <td>Entries</td>
-                    <td>Page</td>
-                    <td class="text-end">
-                            <span class="dropdown">
-                              <button class="btn dropdown-toggle align-text-top" data-bs-boundary="viewport" data-bs-toggle="dropdown">Actions</button>
-                              <div class="dropdown-menu dropdown-menu-end">
-                                <a class="dropdown-item" href="#">
-                                  Action
-                                </a>
-                                <a class="dropdown-item" href="#">
-                                  Another action
-                                </a>
-                              </div>
-                            </span>
-                    </td>
-                </tr>
+                    <tr v-for="payment in payments.data">
+                        <td><input class="form-check-input m-0 align-middle" type="checkbox" aria-label="Select invoice"></td>
+                        <td><span class="text-muted">{{ payment.OrderId }}</span></td>
+                        <td>{{formatDateTime(payment.created_at)}}</td>
+                        <td>{{payment.from}}</td>
+                        <td>{{payment.type}}</td>
+                        <td>БУДЕТ community_title!!!</td>
+                        <td>{{payment.status}}</td>
+                        <td>{{payment.add_balance}}</td>
+                    </tr>
                 </tbody>
             </table>
+        </div>
+        <div v-if="payments.per_page < payments.total" class="card-footer d-flex align-items-center">
+            <p class="m-0 text-muted">Показано <span>{{ payments.per_page }}</span> из <span>{{ payments.total }}</span> записей</p>
+            <ul class="pagination m-0 ms-auto">
+                <li v-for="link in payments.links" class="page-item" :class="{'active' : link.active}"><a class="page-link" @click="setPageByUrl(link.url)" href="#">{{ link.label }}</a></li>
+            </ul>
         </div>
     </div>
 </template>
 
 <script>
+
 export default {
     name: "Payments",
+
     data() {
         return {
             filter_data:{
                 search : null,
-                entries : 10,
+                entries : 5,
                 page : 1,
             }
         }
         
     },
+
     watch: {
         filter_data: {
             deep: true,
             handler: _.debounce(function(v) {
-                console.log(v)
                 this.$store.dispatch('LOAD_PAYMENTS', v);
-                console.log(this.$store)
             },400)
         }
     },
@@ -93,16 +90,20 @@ export default {
     },
     computed: {
         payments() {
-            console.log(this.$store.getters.payments.data)
-            return this.$store.getters.payments;
+            console.log(this.$store.getters.getPayments)
+            return this.$store.getters.getPayments;
         }
     },
     methods:{
-        // setPageByUrl(url){
-        //     if(url){
-        //         this.filter_data.page = getParameterByName('page', url);
-        //     }
-        // }
+        setPageByUrl(url){
+            if(url){
+                this.filter_data.page = getParameterByName('page', url);
+            }
+        },
+        formatDateTime(str){
+            let date = new Date(str);
+            return `${date.toLocaleDateString('ru')} ${date.toLocaleTimeString('ru')}`;
+        }
     }
 }
 </script>
--- resources/admin/js/modules/payments.js
+++ resources/admin/js/modules/payments.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 let state = {
-    _payment: [],
+    _payment: {},
 }
 
 let getters = {
@@ -17,33 +17,33 @@ let mutations = {
 }
 
 let actions = {
-    LOAD_PAYMENTS({commit}, filter_data){
-        return new Promise((resolve, reject) => {
-            //commit('auth_request');
-            axios({url: 'http://tribes/api/v2/payments', data: filter_data, method: 'POST' })
-                .then(resp => {
-                    commit('SET_PAYMENT', resp.data);
-                    resolve(resp);
-                })
-                .catch(err => {
-                    reject(err);
-                })
-        })
-    },
-    // async LOAD_PAYMENTS ({commit}, filter_data) {  
-    //     // console.log(commit);
-    //     // console.log(filter_data);
-    //     try {
-    //         const resp = await axios({
-    //             method: "post",
-    //             url: "http://tribes/api/v2/payments",
-    //             data: filter_data
-    //         })
-    //         commit("SET_PAYMENT", resp.data);
-    //     } catch (error) {
-    //         // console.log(error);
-    //     }
+    // LOAD_PAYMENTS({commit}, filter_data){
+    //     return new Promise((resolve, reject) => {
+    //         //commit('auth_request');
+    //         axios({url: 'http://tribes/api/v2/payments', data: filter_data, method: 'POST' })
+    //             .then(resp => {
+    //                 commit('SET_PAYMENT', resp.data);
+    //                 resolve(resp);
+    //             })
+    //             .catch(err => {
+    //                 reject(err);
+    //             })
+    //     })
     // },
+    async LOAD_PAYMENTS ({commit}, filter_data) {
+        console.log(commit);
+        console.log(filter_data);
+        try {
+            const resp = await axios({
+                method: "post",
+                url: "http://tribes/api/v2/payments",
+                data: filter_data
+            })
+            commit("SET_PAYMENT", resp.data);
+        } catch (error) {
+            console.log(error);
+        }
+    },
 }
 
 export default {
