b71f15ecbdbbb5222019b134549070dcc786e7f9
andrey <adolgopolov0@gmail.com>
2022-08-01 10:10:42 +0300

    TBS-123 media statistic filter api

--- app/Filters/API/MediaSalesFilter.php
+++ app/Filters/API/MediaSalesFilter.php
@@ -2,11 +2,15 @@
 
 namespace App\Filters\API;
 
+use App\Exceptions\StatisticException;
 use App\Helper\ArrayHelper;
+use Illuminate\Database\Query\Builder;
 use Illuminate\Http\Request;
 
 class MediaSalesFilter extends QueryAPIFilter
 {
+    /** @var Builder */
+    protected $builder;
     protected function _sortingName($name): string
     {
         $list = [
@@ -51,5 +55,33 @@ class MediaSalesFilter extends QueryAPIFilter
     public function status($value)
     {
         return $this->builder->where(['payments.status' => $value]);
+    }
+
+    /**
+     * временной период отображаемых данных начало и конец
+     * @param $value
+     * @return Builder
+     * @throws StatisticException
+     */
+    public function createDate($value)
+    {
+        $dates = explode(" - ",$value);
+        if(count($dates) !== 2) {
+            throw new StatisticException('Даты в фильтр передаются в виде "датаНачало - датаКонец"');
+        }
+
+        return $this->builder->whereBetween('m_product_sales.created_at' , $dates);
+    }
+
+    /**
+     * группировка день неделя месяц
+     *
+     * @param $value
+     * @return Builder
+     */
+    public function period($value)
+    {
+        //todo фильтр периода, единица группировки для сетки графика
+        return $this->builder;
     }
 }
