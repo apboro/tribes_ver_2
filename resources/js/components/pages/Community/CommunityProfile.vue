204087325e4102f06dcd6d33129005a8343f642a
Pyatakov Oleg <pyatak.ov@yandex.ru>
2022-08-03 16:07:46 +0300

    add community profile block

--- resources/js/components/pages/Community/CommunityProfile.vue
+++ resources/js/components/pages/Community/CommunityProfile.vue
@@ -19,15 +19,59 @@
         </div>
 
         <div class="profile-community__connect">
-            date
+            <p class="profile-community__connect-label">
+                Дата подключения:
+            </p>
+
+            <p class="profile-community__connect-date">
+                12 july 2022
+            </p>
         </div>
 
-        <div class="profile-community__link">
-            link
+        <div class="profile-community__pay-link-block">
+            <p class="profile-community__pay-link-label">
+                Ссылка на страницу оплаты для доступа к сообществу
+            </p>
+            
+            <div class="profile-community__pay-link-wrapper">
+                <a href="" class="link profile-community__pay-link">
+                    Перейти
+                </a>
+
+                <button class="link profile-community__pay-link profile-community__pay-link--divider">
+                    Скопировать
+                </button>
+            </div>
         </div>
 
         <div class="profile-community__money">
-            money
+            <div>
+                <p class="profile-community__money-label">
+                    Доступный баланс
+                </p>
+
+                <p class="profile-community__money-value">
+                    ₽ 20344,34 <span class="profile-community__money-difference profile-community__money-difference--plus">+1900 ₽</span>
+                </p>
+            </div>
+
+            <div>
+                <p class="profile-community__money-label">
+                    Всего заработано
+                </p>
+
+                <p class="profile-community__money-value">
+                    ₽ 150332,43
+                </p>
+            </div>
+
+            <button class="button-filled profile-community__money-btn">
+                Вывести
+            </button>
+
+            <button class="button-filled profile-community__money-btn button-filled--disabled">
+                Все транзакции
+            </button>
         </div>
     </div>
 </template>
