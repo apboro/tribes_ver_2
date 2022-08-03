88201097adb7805e84abf0b55816af6103954c3c
Pyatakov Oleg <pyatak.ov@yandex.ru>
2022-08-03 13:20:01 +0300

    implement bar,line charts components

---
+++ resources/js/components/ui/chart/LineChart.vue
@@ -0,0 +1,175 @@
+<template>
+  <LineChartGenerator
+    :chart-options="chartOptions"
+    :chart-data="chartData"
+    :chart-id="chartId"
+    :dataset-id-key="datasetIdKey"
+    :plugins="plugins"
+    :css-classes="cssClasses"
+    :styles="styles"
+    :width="width"
+    :height="height"
+  />
+</template>
+
+<script>
+    import { Line as LineChartGenerator } from 'vue-chartjs/legacy';
+    import { Chart as ChartJS, Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale, PointElement } from 'chart.js';
+
+    ChartJS.register(Title, Tooltip, Legend, LineElement, CategoryScale, LinearScale, PointElement);
+
+    export default {
+        name: 'LineChart',
+        components: { LineChartGenerator },
+        props: {
+            chartId: {
+                type: String,
+                default: 'line-chart'
+            },
+
+            datasetIdKey: {
+                type: String,
+                default: 'label'
+            },
+
+            width: {
+                type: Number,
+                default: 400
+            },
+
+            height: {
+                type: Number,
+                default: 400
+            },
+
+            cssClasses: {
+                default: '',
+                type: String
+            },
+
+            styles: {
+                type: Object,
+                default: () => {}
+            },
+
+            plugins: {
+                type: Object,
+                default: () => {}
+            },
+
+            chartData: {
+                type: Object,
+                default: () => [],
+            },
+        },
+
+        data() {
+            return {
+                chartOptions: {
+                    responsive: true,
+                    maintainAspectRatio: false,
+                    //tension: 0.5, //скругление
+                    layout: {
+                        padding: 0
+                    },
+                    
+                    /* scaleLabel: {
+                        display: false
+                    }, */
+                    scales: {
+                        x: {
+                            display: false,
+                            scaleLabel: {
+                                display: true
+                            },
+                            gridLines: {
+                                display: true,
+                                color: 'rgb(100, 0, 0)',
+                                zeroLineColor: 'rgb(0, 100, 0)'
+                            },
+                            ticks: {
+                                fontColor: 'rgb(0, 0, 100)'
+                            }
+                        },
+                        y: {
+                            display: false,
+                            
+                            scaleLabel: {
+                                display: true,
+                                
+                            },
+                            ticks: {
+                                stepSize: 0,
+                                min: 0,
+                                max: 400,
+                                fontColor: 'rgb(0, 0, 100)',
+                                callback: (label, index, labels) => {
+                                    return this.currency
+                                    ?
+                                        new Intl.NumberFormat('ru-RU', { style: 'currency', currency: 'RUB', minimumFractionDigits: 0 }).format(label)
+                                    :
+                                        label 
+                                }
+                            },
+                            gridLines: {
+                                display: true,
+                                color: 'rgb(0, 100, 0)',
+                                zeroLineColor: 'rgb(0, 100, 0)'
+                            }
+                        }
+                    },
+
+                    plugins: {
+                        legend: {
+                            display: false,
+                            position: 'bottom',
+                            align: 'end',
+
+                            /* legendCallback: function(chart) {
+                               console.log(13);
+                            }, */
+                            
+                            labels: {
+                                usePointStyle: true,
+                                padding: 25,
+                                boxWidth: 9
+                            }
+                        },
+                        title: {
+                            display: true,
+                            text: "My Data"
+                        },
+                        
+                        tooltip: {
+                            // Updated default tooltip UI
+                           /*  shadowOffsetX: 1,
+                            shadowOffsetY: 1,
+                            shadowBlur: 8,
+                            shadowColor: 'rgb(0, 0, 0)',
+                            backgroundColor: 'rgb(0, 0, 0)',
+                            titleFontColor: 'rgb(0, 0, 0)',
+                            bodyFontColor: 'rgb(0, 0, 0)',
+                            callbacks: {
+                                label: (label) => {
+                                    return this.currency
+                                    ?
+                                        new Intl.NumberFormat('ru-RU', { style: 'currency', currency: 'RUB', minimumFractionDigits: 0 }).format(label.raw)
+                                    :
+                                        label.raw
+                                }
+                            } */
+                        },
+                    }
+                    
+                },
+
+                
+                
+            }
+        },
+
+        mounted() {
+            /* this.styles = "padding: 50px 0 0 0" */
+        }
+    }
+</script>
--- resources/js/pages/Statistics.vue
+++ resources/js/pages/Statistics.vue
@@ -1,13 +1,151 @@
 <template>
     <div class="statistics">
         <div class="container">
-            i am statistics
+            <!-- HEAD -->
+            <header class="community__header" style="display: flex; justify-content: space-between;">
+                <h2 class="community__title">Title</h2>
+
+                <nav class="community__nav">
+                    <ul style="display: flex; column-gap: 20px;">
+                        <li>
+                            <a href="">Аналитика</a>
+                        </li>
+
+                        <li>
+                            <a href="">Донаты</a>
+                        </li>
+
+                        <li>
+                            <a href="">Тарифы</a>
+                        </li>
+
+                        <li>
+                            <a href="">База знаний</a>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+
+            <!-- Profile -->
+            <div class="community__profile">
+                Community profile
+            </div>
+
+            <!-- Analytics -->
+            <div class="analytics">
+
+                <div>
+                    <line-chart
+                        :chartData="chartData2"
+                    />
+
+                    <div class="legend-box" style="display: flex;">
+                        <button
+                            style="padding: 10px;"
+                            :style="{ backgroundColor: dataset.backgroundColor }"
+                            v-for="(dataset, index) in chartData2.datasets"
+                            :key="index"
+                            @click="toggleData(index)"
+                        >
+                            {{ dataset.label }}
+                        </button>
+                    </div>
+                </div>
+
+                <ul class="analytics__list" style="display: flex; column-gap: 20px;">
+                    <li class="analytics__item" style="width: 500px; ">
+                        <bar-chart
+                            :chartData="chartData"
+                            
+                        />
+                    </li>
+
+                    <li class="analytics__item" style="width: 500px; ">
+                        <line-chart
+                            :chartData="chartData2"
+                        />
+
+                        <div class="legend-box" style="display: flex;">
+                            <button
+                                style="padding: 10px;"
+                                :style="{ backgroundColor: dataset.backgroundColor }"
+                                v-for="(dataset, index) in chartData2.datasets"
+                                :key="index"
+                                @click="toggleData(index)"
+                            >
+                                {{ dataset.label }}
+                            </button>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+    import BarChart from '../components/ui/chart/BarChart.vue';
+    import LineChart from '../components/ui/chart/LineChart.vue';
+
     export default {
-        name: 'Statistics'
+        name: 'Statistics',
+        
+        components: { BarChart, LineChart },
+
+        data() {
+            return {
+                chartData: {
+                    labels: [ 'January', 'February', 'March', '1', '2', '3' ],
+                    datasets: [ { data: [40, 20, 12, 1, 2, 3] } ]
+                },
+
+                chartData2: {
+                    labels: [ '1', '2', '3' ],
+                    datasets: [
+                        {
+                            label: "Data 1",
+                            data: [2, 20, 5],
+                            backgroundColor: "rgba(171, 71, 188, 1)",
+                            fontColor: 'red',
+                            borderColor: "rgba(1, 116, 188, 0.50)",
+                            pointBackgroundColor: "rgba(171, 71, 188, 1)",
+                            pointRadius: 0,
+                            hidden: false,
+                            //borderWidth: 10,
+                        },
+
+                        {
+                            label: "Data 2",
+                            data: [20, 100, 50],
+                            backgroundColor: "yellow",
+                            borderColor: "rgba(0, 116, 0, 0.50)",
+                            pointBackgroundColor: "yellow",
+                            pointRadius: 0,
+                            hidden: false,
+                        }
+                    ]
+                },
+            }
+        },
+
+        methods: {
+            toggleData(value) {    
+                const visibilityData = this.chartData2.datasets[value].hidden;
+            
+                if (visibilityData) {
+                    this.chartData2.datasets[value].hidden = false;
+                } else {
+                    this.chartData2.datasets[value].hidden = true;
+                }
+            }
+        },
+
+        mounted() {
+            
+        },
     }
 </script>
+
+<style lang="scss" scoped>
+
+</style>
