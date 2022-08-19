39f9649c2fde7bcd3f82e474554ddca50f3bd33f
mda <spAm250799@gmail.com>
2022-08-19 15:28:26 +0300

    add dockerfile, nginx configs and .gitlab-ci.yaml

---
+++ .docker/docker-entrypoint.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+auto_envsubst() {
+  local template_dir="${NGINX_ENVSUBST_TEMPLATE_DIR:-/etc/nginx/templates}"
+  local suffix="${NGINX_ENVSUBST_TEMPLATE_SUFFIX:-.template}"
+  local output_dir="${NGINX_ENVSUBST_OUTPUT_DIR:-/etc/nginx/configs}"
+
+  local template defined_envs relative_path output_path subdir
+  defined_envs=$(printf '${%s} ' $(env | cut -d= -f1))
+  [ -d "$template_dir" ] || return 0
+  find "$template_dir" -follow -type f -name "*$suffix" -print | while read -r template; do
+    relative_path="${template#$template_dir/}"
+    output_path="$output_dir/${relative_path%$suffix}"
+    subdir=$(dirname "$relative_path")
+    # create a subdirectory where the template file exists
+    mkdir -p "$output_dir/$subdir"
+    echo "Running envsubst on $template to $output_path"
+    envsubst "$defined_envs" < "$template" > "$output_path"
+  done
+}
+
+command="$1"
+
+case "$command" in
+  "php-fpm" )
+    php-fpm
+  ;;
+  "frontend" | "proxy" )
+    ln -sf /dev/stdout /var/log/nginx/access.log
+    ln -sf /dev/stderr /var/log/nginx/error.log
+    auto_envsubst
+    conf_file="$command.conf"
+    conf_source="/etc/nginx/configs"
+    conf_target="/etc/nginx/http.d"
+    [ ! -e "$conf_source/$conf_file" ] && echo "No configuration file found for $command mode" && exit 1
+    ln "$conf_source/$conf_file" "$conf_target/$conf_file"
+    nginx -g "daemon off;"
+  ;;
+  * )
+    php $@
+  ;;
+esac
---
+++ Dockerfile
@@ -0,0 +1,42 @@
+ARG COMPOSER_VERSION="2.1.5"
+ARG PHP_VERSION="7.4.30"
+ARG NODE_VERSION="14.20"
+ARG NGINX_VERSION="1.23.1"
+ARG WORKDIR="/app"
+
+FROM node:${NODE_VERSION} as frontend-builder
+ARG WORKDIR
+WORKDIR ${WORKDIR}
+COPY package.json package-lock.json ${WORKDIR}/
+RUN npm ci
+COPY public ${WORKDIR}/public/
+COPY resources ${WORKDIR}/resources/
+COPY *.js artisan ${WORKDIR}/
+# COPY app ${WORKDIR}/app/
+RUN npm run prod
+
+FROM php:${PHP_VERSION}-fpm-alpine
+ARG WORKDIR
+WORKDIR ${WORKDIR}
+
+RUN apk add --no-cache nginx gettext tzdata ca-certificates && rm /etc/nginx/http.d/*
+
+COPY .docker/docker-entrypoint.sh /
+COPY .docker/*.conf.template /etc/nginx/templates/
+
+ENTRYPOINT ["/docker-entrypoint.sh"]
+CMD [""]
+
+# Graceful shutdown for nginx
+STOPSIGNAL SIGQUIT
+
+# nginx and php-fpm
+EXPOSE 80 9000
+
+COPY composer.* .env.example artisan ${WORKDIR}/
+RUN cp -v .env.example .env && \
+    php composer.phar install --no-dev --no-autoloader --prefer-dist
+
+COPY --chown=www-data:www-data . ${WORKDIR}/
+RUN php composer.phar install --no-dev --prefer-dist
+COPY --chown=www-data:www-data --from=frontend-builder ${WORKDIR}/public ${WORKDIR}/public/
