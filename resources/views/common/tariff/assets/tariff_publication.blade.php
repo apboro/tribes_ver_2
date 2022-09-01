1218d3889b717e74c4791597ab0fb693d9321010
Pyatakov Oleg <pyatak.ov@yandex.ru>
2022-09-01 14:09:14 +0300

    TBS-364 add crop image blok styles

--- resources/views/common/tariff/assets/tariff_publication.blade.php
+++ resources/views/common/tariff/assets/tariff_publication.blade.php
@@ -1,129 +1,130 @@
-<div>                                   
-    <div class="row">
-        <!-- Текст -->
-        <div class="col-md-12 col-lg-6">
-            <div class="mb-1 mb-lg-0">
-                <label class="form-label pointer" for="publication_description">
-                    {{ __('tariff.description_publication_tariffs') }}
-                </label>
+<div class="settings-text-image community-settings__text-image">
+    <!-- Текст -->
+    <div class="settings-text-image__text">
+        <label class="form-label-red" for="publication_description">
+            {{ __('tariff.description_publication_tariffs') }}
+        </label>
 
-                <textarea
-                    class="form-control @error('publication_description') error @enderror"
-                    id="publication_description"
-                    name="publication_description"
-                    rows="5"
-                    placeholder="{{ __('form.message_text') }}"
-                >@if($community->tariff && $community->tariff->publication_description){{$community->tariff->publication_description}}@endif</textarea>
-                
-                <span class="badge bg-warning hide" title="{{ __('base.unsaved_data') }}">
-                    <i data-feather='save' class="font-medium-1" ></i>
-                </span>
-            </div>
-            @error('publication_description')
-                <div class="alert alert-danger">{{ $message }}</div>
-            @enderror
-        </div>
+        <textarea
+            class="form-control-red @error('publication_description') form-control-red--danger @enderror"
+            id="publication_description"
+            name="publication_description"
+            rows="5"
+            placeholder="Доступные тарифы"
+        >@if($community->tariff && $community->tariff->publication_description){{$community->tariff->publication_description}}@endif</textarea>
+        
+        <span class="badge bg-warning hide" title="{{ __('base.unsaved_data') }}">
+            <i data-feather='save' class="font-medium-1" ></i>
+        </span>
+        
+        @error('publication_description')
+            <div class="form-message form-message--danger">{{ $message }}</div>
+        @enderror
+    </div>
 
-        <!-- Изображение -->
-        <div class="col-md-12 col-lg-6">
-            <label class="form-label">
-                {{ __('base.image') }}
-            </label>
+    <!-- Изображение -->
+    <div class="settings-text-image__image-block" data-crop-image-container="publication">
+        <label class="form-label-red">
+            {{ __('base.image') }}
+        </label>
 
-            <div class="d-flex flex-column align-items-center" data-crop-image-container="publication">
-                <div class="col-12 message-alert hide" data-image-alert></div>
+        <div class="settings-text-image__image" data-crop-image-container="publication">
+            <div class="settings-text-image__image-message hide" data-image-alert></div>
 
-                <!-- Если есть загруженное изображение -->
-                <div
-                    class="col-12 d-flex flex-column position-relative active-image @if($community->tariff && $community->tariff->getPublicationImage()) d-block @else d-none @endif"
-                    data-crop-image-default-image
+            <!-- Если есть загруженное изображение -->
+            <div
+                class="settings-text-image__active-image @if($community->tariff && $community->tariff->getPublicationImage()) d-block @else d-none @endif"
+                data-crop-image-default-image
+            >
+                <img
+                    src="@if($community->tariff && $community->tariff->getPublicationImage()){{ $community->tariff->getPublicationImage()->url }}@endif"
+                    alt=""
                 >
-                    <img
-                        src="@if($community->tariff && $community->tariff->getPublicationImage()){{ $community->tariff->getPublicationImage()->url }}@endif"
-                        alt=""
-                        class="active-image__img rounded w-100"
+            </div>
+
+            <!-- Загрузка изображения -->
+            <div class="settings-text-image__load-container @if($community->tariff &&  $community->tariff->getPublicationImage()) hide @else  @endif" data-crop-image-load-container>
+                    <!-- Описание загрузки -->
+                    <div
+                        class="settings-text-image__instructions"
+                        data-crop-image-instructions
                     >
-                </div>
+                        <p class="settings-text-image__instructions-text">
+                            {{ __('form.drag_image_instruction') }}
+                        </p>
 
-                <!-- Загрузка изображения -->
-                <div class="d-flex flex-column align-items-center w-100 text-center @if($community->tariff &&  $community->tariff->getPublicationImage()) hide @else  @endif" data-crop-image-load-container>
-                    <div class="position-relative mx-auto w-100">
-                        <!-- Описание загрузки -->
-                        <div data-crop-image-instructions>
-                            <p class="mb-1">
-                                {{ __('form.drag_image_instruction') }}
-                            </p>
+                        <p class="settings-text-image__instructions-text settings-text-image__instructions-formats">
+                            {{ __('form.support_format') }}
+                        </p>
 
-                            <small>
-                                {{ __('form.support_format') }}
-                            </small>
-
-                            <strong class="d-block">
-                                {{ __('form.max_image_size') }}
-                            </strong>
-                            
-                            <label
-                                for="publication_image_upload"
-                                class="position-absolute top-0 end-0 bottom-0 start-0 pointer"
-                            ></label>
-                        </div>
-                        <!-- Данные -->
-                        <div class="hide" data-crop-image-data-container>
-                            <input
-                                type="file"
-                                id='publication_image_upload'
-                                type="file"
-                                accept="image/png,image/jpeg,image/gif"
-                                name="files[publication][image]"
-                                onchange="CommunityPage.tariffPagePublications.messageBlock.croppImageControllerPublication.onChange()"
-                                data-crop-image-file
-                            >
-                            
-                            <input
-                                type="hidden"
-                                name="files[publication][delete]"
-                                value="false"
-                                data-crop-image-remove-data
-                            >
-
-                            <input
-                                type="hidden"
-                                name="files[publication][crop]"
-                                value=""
-                                data-crop-image-crop-data
-                            >
-                        </div>  
-
-                        <!-- Croppr -->
-                        <div class="cropper">
-                            <div class="cropper-container" data-crop-image-croppr-container></div>
-                        </div>
-                    </div>
-                </div>
-
-                <!-- Кнопки -->
-                <div class="col-12 d-flex mt-1 @if($community->tariff && $community->tariff->getPublicationImage()) @else hide @endif" data-crop-image-buttons-container>
-                    <div class="input-group">                
+                        <p class="settings-text-image__instructions-text settings-text-image__instructions-text--bold settings-text-image__instructions-size">
+                            {{ __('form.max_image_size') }}
+                        </p>
+                        
                         <label
-                            class="col-7 col-lg-8 btn btn-info px-1"
-                            for="publication_image_upload"                                
-                        >
-                            <i data-feather='download' class="font-medium-1"></i>
-                            <span class="d-none d-xl-inline-block">
-                                {{ __('base.select_image') }}
-                            </span>
-                        </label>
-                    
-                        <span
-                            class="col-5 col-lg-4 btn btn-danger px-1"
-                            onclick="CommunityPage.tariffPagePublications.messageBlock.croppImageControllerPublication.removeLoadedImage()"
-                        >
-                            <i data-feather='trash' class="font-medium-1"></i>    
-                        </span>
+                            for="publication_image_upload"
+                            class="settings-text-image__instructions-label"
+                        ></label>
                     </div>
-                </div>
+                    <!-- Данные -->
+                    <div class="hide" data-crop-image-data-container>
+                        <input
+                            type="file"
+                            id='publication_image_upload'
+                            type="file"
+                            accept="image/png,image/jpeg,image/gif"
+                            name="files[publication][image]"
+                            onchange="CommunityPage.tariffPagePublications.messageBlock.croppImageControllerPublication.onChange()"
+                            data-crop-image-file
+                        >
+                        
+                        <input
+                            type="hidden"
+                            name="files[publication][delete]"
+                            value="false"
+                            data-crop-image-remove-data
+                        >
+
+                        <input
+                            type="hidden"
+                            name="files[publication][crop]"
+                            value=""
+                            data-crop-image-crop-data
+                        >
+                    </div>  
+
+                    <!-- Croppr -->
+                    <div class="settings-text-image__cropper  cropper">
+                        <div class="settings-text-image__cropper-container" data-crop-image-croppr-container></div>
+                    </div>
+                
+            </div>
+
+            <!-- Кнопки -->
+            <div class="settings-text-image__buttons @if($community->tariff && $community->tariff->getPublicationImage()) @else hide @endif" data-crop-image-buttons-container>                
+                <label
+                    class="button-empty button-empty--primary"
+                    for="publication_image_upload"                                
+                >
+                    <span class="d-none d-xl-inline-block">
+                        {{ __('base.select_image') }}
+                    </span>
+                </label>
+            
+                <span
+                    class="button-empty button-empty--primary"
+                    onclick="CommunityPage.tariffPagePublications.messageBlock.croppImageControllerPublication.removeLoadedImage()"
+                >
+                    Удалить    
+                </span>
             </div>
         </div>
     </div>
-    <hr />
-</div>
+</div>    
+
+
+
+
+
+
+
