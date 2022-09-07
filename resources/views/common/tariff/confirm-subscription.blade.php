b1d22d623d423c21bc00dc4f9250e28e0b1f434e
Olesya <olesyabrazhnikova@ukr.net>
2022-09-07 14:06:40 +0300

    added structure and styles for the page confirmed-subscription

--- resources/views/common/tariff/confirm-subscription.blade.php
+++ resources/views/common/tariff/confirm-subscription.blade.php
@@ -8,9 +8,9 @@
             <img class="confirmation_subscription__avatar" src="/images/photo.png" alt="photo of subscriber">
             <div class="confirmation_subscription__header-col1--right">
                 <h4 class="confirmation_subscription__header-col1-title">Tech in UK</h4>
-                <p class="confirmation_subscription__name-value">
-                    <span class="confirmation_subscription__name col1">Подписчиков:</span>
-                    <span class="confirmation_subscription__subscribers">300.5К</span>
+                <p class="confirmation_subscription__name-value subscribers">
+                    <span class="col1">Подписчиков:</span>
+                    <span>300.5К</span>
                 </p>
             </div>
         </div>
@@ -27,7 +27,44 @@
         </div>
 
     </div>
-    <div class="confirmation_subscription__body"></div>
-    <div class="confirmation_subscription__footer"></div>
+    <div class="confirmation_subscription__body">
+        <div class="confirmation_subscription__confirm-check">
+            <input class="confirmation_subscription__confirm-check-input" id="all_rights_check" data-checked="false" type="checkbox">
+            <label class="confirmation_subscription__confirm-check-label" for="all_rights_check">Подтверждаю ознакомление и согласие с <span class="text-primary">правилами</span></label>
+        </div>
+        <div class="confirmation_subscription__confirm-check-rights" id="confirm-check-rights">
+            <div class="confirmation_subscription__confirm-check">
+                <input class="confirmation_subscription__confirm-check-input" data-checked="false" type="checkbox">
+                <a class="confirmation_subscription__btn-link" target="_blank">Правила пользования</a>
+            </div>
+            <div class="confirmation_subscription__confirm-check">
+                <input class="confirmation_subscription__confirm-check-input" data-checked="false" type="checkbox">
+                <a class="confirmation_subscription__btn-link" target="_blank">Политика обработки персональных данных</a>
+            </div>
+            <div class="confirmation_subscription__confirm-check">
+                <input class="confirmation_subscription__confirm-check-input" data-checked="false" type="checkbox">
+                <a class="confirmation_subscription__btn-link" target="_blank">Согласие на обработку персональных данных</a>
+            </div>
+            <div class="confirmation_subscription__confirm-check">
+                <input class="confirmation_subscription__confirm-check-input" data-checked="false" type="checkbox">
+                <a class="confirmation_subscription__btn-link" target="_blank">Согласие на получение информационной рассылки</a>
+            </div>
+            <div class="confirmation_subscription__confirm-check">
+                <input class="confirmation_subscription__confirm-check-input" data-checked="false" type="checkbox">
+                <a class="confirmation_subscription__btn-link" target="_blank">Согласие на подписку</a>
+            </div>
+            <div class="confirmation_subscription__confirm-check">
+                <input class="confirmation_subscription__confirm-check-input" data-checked="false" type="checkbox">
+                <a class="confirmation_subscription__btn-link" target="_blank">Агентский договор (публичная оферта)</a>
+            </div>
+        </div>
+    </div>
+    <div class="confirmation_subscription__footer">
+        <div>
+            <label class="confirmation_subscription__email-label" for="email">Email*</label>
+            <input class="confirmation_subscription__email-input" id="email" placeholder="ivan@moyapochta.ru" name="email" required="true">
+        </div>
+        <a class="button-filled button-filled--primary" href="">Оплатить</a>
+    </div>
 </div>
 @endsection
