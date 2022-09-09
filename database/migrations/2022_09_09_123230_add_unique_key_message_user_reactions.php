1a39de261f19faed96c3b22a9d578c856ba9a9dc
andrey <adolgopolov0@gmail.com>
2022-09-09 14:34:22 +0300

    TBS-420 fix reglament.md fix unique TelegramStatisticSeeder.php проблема неуникальных персонифицированных реакций под сообщением

---
+++ database/migrations/2022_09_09_123230_add_unique_key_message_user_reactions.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUniqueKeyMessageUserReactions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('telegram_message_reactions', function (Blueprint $table) {
+            $table->unique(['telegram_user_id', 'message_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('telegram_message_reactions', function (Blueprint $table) {
+            $table->dropUnique(['telegram_user_id', 'message_id']);
+        });
+    }
+}
