3359f710d5afd611b888ed7522365fdef689af78
Евгений Строй <evgenijst21@gmail.com>
2022-09-14 16:16:38 +0300

    edit jobs for new users

--- app/Jobs/SetNewTelegramUsers.php
+++ app/Jobs/SetNewTelegramUsers.php
@@ -11,6 +11,7 @@ use Illuminate\Queue\SerializesModels;
 use App\Services\Telegram\MainComponents\Madeline;
 use App\Models\TelegramUser;
 use App\Models\Community;
+use App\Services\Telegram\TelegramMtproto\UserBot;
 
 class SetNewTelegramUsers implements ShouldQueue
 {
@@ -40,28 +41,95 @@ class SetNewTelegramUsers implements ShouldQueue
             $query->where('chat_id', $this->chatId);
         })->first();
 
-        $pwr_chat = Madeline::settings()->getPwrChat($this->chatId);
-        $existUsers = [];
-        foreach ($pwr_chat['participants'] as $user) {
-            if ($user['role'] !== 'banned' && $user['user']['type'] !== 'bot') {
-                $existUsers[] = $user;
+        $userBot = new UserBot;
+        $connection = $community->connection;
+        $limit = 100;
+        $offset = 0;
+        $chat_id = str_replace('-', '',(str_replace(-100, '', $connection->chat_id)));
+        if ($connection && $connection->is_there_userbot === true) {
+            if ($connection->access_hash !== null) {
+                
+                $participants = $userBot->getUsersInChannel($chat_id, $connection->access_hash, $limit, $offset);
+                $this->getChannelUsers($community, $participants);
+                $count = $participants[0]->users->count;
+                if ($count > $limit) {
+                    $offset = $limit;
+                    for ($i = 0; $i <= ceil($count / $limit); $i++) {
+                        $participants = $userBot->getUsersInChannel($chat_id, $connection->access_hash, $limit, $offset);
+                        $this->getChannelUsers($community, $participants);
+                        $offset += $limit;
+                    }
+                }
+            } else {
+                $participants = $userBot->getChatInfo($chat_id);
+                $this->getUsersForGroup($community, $participants);
             }
         }
-        foreach ($existUsers as $user) {
 
-            $ty = TelegramUser::firstOrCreate([
-                'telegram_id' => $user['user']['id']
-            ]);
+    }
 
-            $ty->auth_date   = isset($user['date']) ? $user['date'] : NULL;
-            $ty->user_name  = isset($user['user']['username']) ? $user['user']['username'] : NULL;
-            $ty->first_name  = isset($user['user']['first_name']) ? $user['user']['first_name'] : NULL;
-            $ty->last_name   = isset($user['user']['last_name']) ? $user['user']['last_name'] : NULL;
-            $ty->save();
-
-            if (!$ty->communities()->find($community->id))
-                $ty->communities()->attach($community);
-
+    protected function getChannelUsers($community, $participants)
+    {
+        $newParticipants = $participants[0]->users->participants;
+        $users = $participants[0]->users->users;
+        foreach ($newParticipants as $participant) {
+            foreach ($users as $user) {
+                $role = $this->getChannelRole($participant);
+                if ($participant->user_id === $user->id)
+                    $this->saveUser($community, $user, $participant->date ?? null, $role);
+            }
         }
+    }
+
+    protected function getChannelRole($participant) 
+    {
+        if ($participant->_ == 'channelParticipantAdmin')
+            $role = 'admin';
+        elseif ($participant->_ == 'channelParticipantCreator')
+            $role = 'creator';
+        else 
+            $role = 'member';
+
+        return $role;
+    }
+
+    protected function getUsersForGroup($community, $participants)
+    {
+        $newParticipants = $participants[0]->chatInfo->full_chat->participants->participants;
+        $users = $participants[0]->chatInfo->users;
+
+        foreach ($newParticipants as $participant) {
+            foreach ($users as $user) {
+                $role = $this->getGroupRole($participant);
+                if ($participant->user_id === $user->id)
+                    $this->saveUser($community, $user, $participant->date ?? null, $role);
+            }
+        }
+    }
+
+    protected function getGroupRole($participant) 
+    {
+        if ($participant->_ == 'chatParticipantAdmin')
+            $role = 'admin';
+        elseif ($participant->_ == 'chatParticipantCreator')
+            $role = 'creator';
+        else 
+            $role = 'member';
+
+        return $role;
+    }
+
+    protected function saveUser($community, $user, $accession_date, $role)
+    {
+        $ty = TelegramUser::firstOrCreate([
+            'telegram_id' => $user->id
+        ]);
+        $ty->user_name  = $user->username ?? NULL;
+        $ty->first_name  = $user->first_name ?? NULL;
+        $ty->last_name   = $user->last_name ?? NULL;
+        $ty->save();
+
+        if (!$ty->communities()->find($community->id))
+            $ty->communities()->attach($community, ['role' => $role, 'accession_date' => $accession_date ?? null]);
     }
 }
