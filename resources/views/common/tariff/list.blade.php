16c8c752d47996eebfe3687b108e6d1a44bf3dfa
andrey <adolgopolov0@gmail.com>
2022-09-21 16:54:55 +0300

    TBS-392 добавил инлайн команды для тарифа и его вариантов по отдельности

--- resources/views/common/tariff/list.blade.php
+++ resources/views/common/tariff/list.blade.php
@@ -72,7 +72,9 @@
                 </option>
             </select>
         </div>
-        
+        <div>
+            <p>{{$community->tariff()->first()->getInlineLink()??''}}</p>
+        </div>
         <!-- TARIFF LIST -->
         @if(isset($tariffs[0]))
         <ul class="community-tariff__list">
