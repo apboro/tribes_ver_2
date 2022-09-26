a82e4398791cad3d274a29834dd4326e2f43d6b0
andrey <adolgopolov0@gmail.com>
2022-09-26 15:50:06 +0300

    TBS-508 ссылка на посадочную страницу для одного варианта тарифа

--- app/Http/Controllers/TariffController.php
+++ app/Http/Controllers/TariffController.php
@@ -136,7 +136,7 @@ class TariffController extends Controller
         }
         $this->tariffRepo->statisticView($request, $community);
 
-        return view('common.tariff.index')->withCommunity($community,$inline_link);
+        return view('common.tariff.index')->with('community', 'inline_link');
     }
 
     public function tariff(Community $community)
