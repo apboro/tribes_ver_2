54fb086a46fd01a6fe327018471a90c754b42e37
tracer2008 <mihail@webstyle.top>
2022-09-27 17:57:29 +0300

    TBS-394 исправил выборку

--- app/Services/Tariff/TariffService.php
+++ app/Services/Tariff/TariffService.php
@@ -20,7 +20,7 @@ class TariffService {
         $this->telegramMainBotService = $telegramMainBotService;
     }
 
-    public function sendMessageAboutDeactivatedTariff()
+    /*public function sendMessageAboutDeactivatedTariff()
     {
         $collectionRecords = DB::table( DB::raw("telegram_users_community as tuc") )
             ->join( DB::raw("telegram_users as tu"),'tuc.telegram_user_id','=','tu.telegram_id' )
@@ -28,7 +28,6 @@ class TariffService {
             ->join( DB::raw("telegram_users_tarif_variants as tutv"),'tu.id','=','tutv.telegram_user_id' )
             ->join( DB::raw("tarif_variants as tv"),'tv.id','=','tutv.tarif_variants_id' )
             ->leftJoin( DB::raw("tariffs as t"),'c.id','=','t.community_id' )
-
             ->leftJoin( DB::raw("tarif_variants as tvc"),'tvc.tariff_id','=','t.id' )
             ->select([
                 DB::raw("tu.user_id as user_id"),
@@ -45,6 +44,53 @@ class TariffService {
 
         $user_id = $collectionRecords->pluck('user_id')->all();
         $telegram_user_id = $collectionRecords->pluck('user_id')->all();
+        $community_id = $collectionRecords->pluck('community_id')->all();
+        $tvc_ids = $collectionRecords->pluck('tvc_ids')->all();
+        $tvc_ids_arr = array_map(function ($item) {
+            $item = trim($item, '{}');
+            return explode(',', $item);
+        },$tvc_ids);
+        $tvc_ids_arr = array_reduce($tvc_ids_arr, 'array_merge', []);
+        $tvc_ids_arr = array_filter ($tvc_ids_arr);
+
+        $users = ArrayHelper::index(User::whereIn('id', $user_id)->get(), 'id');
+        $telegram_users = ArrayHelper::index(TelegramUser::whereIn('telegram_id', $telegram_user_id)->get(), 'user_id') ;
+        $communities = ArrayHelper::index(Community::whereIn('id', $community_id)->get(), 'id') ;
+        $tarVars = ArrayHelper::index(TariffVariant::whereIn('id', $tvc_ids_arr)->get(), 'id') ;
+
+        $this->sendMessagesEmailBot($collectionRecords, $users, $telegram_users, $communities);
+    }*/
+
+    public function sendMessageAboutDeactivatedTariff()
+    {
+        $collectionRecords = DB::table( DB::raw("telegram_users_tarif_variants as tutv") )
+        ->join( DB::raw("tarif_variants as tv"), 'tv.id', '=', 'tutv.tarif_variants_id' )
+        ->join( DB::raw("tariffs as t"), 't.id', '=', 'tv.tariff_id' )
+        ->leftJoin( DB::raw("tarif_variants as tvc"), 'tvc.tariff_id', '=', 't.id' )
+
+        ->join( DB::raw("telegram_users as tu"), 'tu.id', '=', 'tutv.telegram_user_id' )
+        ->join( DB::raw("users as u"), 'u.id', '=', 'tu.user_id' )
+
+        ->join( DB::raw("communities as c"),'c.id','=','t.community_id' )
+
+        ->select([
+            DB::raw("u.id as user_id"),
+            DB::raw("tu.telegram_id as telegram_user_id"),
+            DB::raw("c.id as community_id"),
+            DB::raw("coalesce(array_agg(tvc.id) FILTER ( WHERE tvc.\"isActive\"=true AND tvc.\"isPersonal\"=false),'{}') as tvc_ids"),
+        ])
+
+        ->orWhereRaw("tv.\"isActive\" = false AND tv.period < 4 AND tutv.days = 1")
+        ->orWhereRaw("tv.\"isActive\" = false AND tv.period > 3 AND tv.period < 8 AND tutv.days in(3,2,1)")
+        ->orWhereRaw("tv.\"isActive\" = false AND tv.period > 7 AND tutv.days in(7,3,2,1)")
+
+        ->groupBy("u.id","tu.telegram_id", "c.id")
+
+        ->get();
+
+
+        $user_id = $collectionRecords->pluck('user_id')->all();
+        $telegram_user_id = $collectionRecords->pluck('telegram_user_id')->all();
         $community_id = $collectionRecords->pluck('community_id')->all();
         $tvc_ids = $collectionRecords->pluck('tvc_ids')->all();
         $tvc_ids_arr = array_map(function ($item) {
@@ -90,7 +136,6 @@ class TariffService {
                     new Mailer('Сервис TRIBES', $textMessage, 'Заканчивается тариф ', $user->email);
                 }
             }
-
         }
     }
 
