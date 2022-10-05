4c54186dd9eac5612c3fcf6af4799cf15aedb4fd
Pyatakov Oleg <pyatak.ov@yandex.ru>
2022-10-05 16:06:42 +0300

    TBS-401 change svg icon

--- resources/admin/js/components/pages/Users.vue
+++ resources/admin/js/components/pages/Users.vue
@@ -38,7 +38,7 @@
                             </template>
                             
                             <template v-if="sortRuleOnDate == 'asc'">
-                                <svg style="margin: 0; transform: rotate(180deg)" xmlns="http://www.w3.org/2000/svg" class="icon icon-sm text-dark icon-thick" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><polyline points="6 15 12 9 18 15"></polyline></svg>
+                                <svg style="margin: 0;" xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><polyline points="6 9 12 15 18 9"></polyline></svg>
                             </template>
                         </button>    
                     </th>
