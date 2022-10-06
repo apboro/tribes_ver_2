248a05358eb3e4cde4420846d618da35b536c7b5
Pyatakov Oleg <pyatak.ov@yandex.ru>
2022-10-06 16:37:14 +0300

    TBS-599 reserve project list menu, new toast style

--- resources/views/common/template/header.blade.php
+++ resources/views/common/template/header.blade.php
@@ -35,6 +35,8 @@
         
         @include('common.template.assets.main_menu_new')
 
+        <!-- @include('common.template.assets.project_list_mobile') -->
+
         <div class="main-header-red__auxiliary">
             <i class="main-header-red__search">
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
