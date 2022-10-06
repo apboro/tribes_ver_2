141a31031c6db5b78e9c51cb4309f12e3069758c
Olesya <olesyabrazhnikova@ukr.net>
2022-10-06 15:18:04 +0300

    TBS-419 chaned commits

--- resources/js/plugins/Project.js
+++ resources/js/plugins/Project.js
@@ -25,11 +25,13 @@ export default class Project extends Page {
         let qty = document.getElementById('qty_another_projects');
         qty.textContent = `0`;
         let totalQty = document.querySelectorAll('.project-creation__list-communities .profile__list .profile__item-wrap').length;
+        
         //удаления дефолтного блока с текстом, если в сообществах проекта есть элементы
         let emptyBlock = document.querySelector('.project-creation__project-main--empty');
         if(emptyBlock !== null && totalQty > 0) {
             emptyBlock.remove();
         }
+
         //изменение количества всех сообществ (checked && !checked)
         let qtyCommunitiesInProject = document.getElementById('qty_of_communities_in_project')
         qtyCommunitiesInProject.textContent = `${totalQty}`
