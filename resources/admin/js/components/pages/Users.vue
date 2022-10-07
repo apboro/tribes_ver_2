3166b32c9096a1509076012c1d2677044efa6b65
Pyatakov Oleg <pyatak.ov@yandex.ru>
2022-10-07 14:12:29 +0300

    TBS-400 add email field to users

--- resources/admin/js/components/pages/Users.vue
+++ resources/admin/js/components/pages/Users.vue
@@ -26,6 +26,7 @@
                         <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-sm text-dark icon-thick" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><polyline points="6 15 12 9 18 15"></polyline></svg>
                     </th>
                     <th>Имя</th>
+                    <th>Почта</th>
                     <th>Телефон</th>
                     <th>
                         Создан
@@ -56,7 +57,7 @@
                             </template>
                         </i>    
                     </th>
-                    <th>Комиссия</th>
+                    <th>Комиссия, %</th>
                     <th></th>
                 </tr>
                 </thead>
