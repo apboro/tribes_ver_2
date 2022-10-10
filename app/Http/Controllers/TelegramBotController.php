05ecbcbb444543bdcaa27490598b41361e2602cf
Евгений Строй <evgenijst21@gmail.com>
2022-10-10 15:33:37 +0300

    stop bot

--- app/Http/Controllers/TelegramBotController.php
+++ app/Http/Controllers/TelegramBotController.php
@@ -28,12 +28,12 @@ class TelegramBotController extends Controller
         ]);
         $botName = config('telegram_bot.bot.botName');
 
-        if (env('GRAB_TEST_DATA') === true) {
-            $time = time();
-            $acosData = json_decode($data, true);
-            $updId = $acosData['update_id'] ?? '';
-            Storage::disk('telegram_data')->put("message_{$botName}_upd{$updId}_{$time}.json", $data);
-        }
+        // if (env('GRAB_TEST_DATA') === true) {
+        //     $time = time();
+        //     $acosData = json_decode($data, true);
+        //     $updId = $acosData['update_id'] ?? '';
+        //     Storage::disk('telegram_data')->put("message_{$botName}_upd{$updId}_{$time}.json", $data);
+        // }
 
         $this->mainBotService->run($botName, $request->collect());
     }
