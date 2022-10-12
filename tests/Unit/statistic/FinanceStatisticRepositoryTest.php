89d9dfa1de735833edbaee9aa84f247653e264bb
andrey <adolgopolov0@gmail.com>
2022-10-12 14:39:35 +0300

    TBS-603 FinanceStatisticControllerTest.php

---
+++ tests/Unit/statistic/FinanceStatisticRepositoryTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit\statistic;
+
+use PHPUnit\Framework\TestCase;
+
+class FinanceStatisticRepositoryTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/Unit/statistic/TeleMessageStatisticRepositoryTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit\statistic;
+
+use PHPUnit\Framework\TestCase;
+
+class TeleMessageStatisticRepositoryTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $this->assertTrue(true);
+    }
+}
