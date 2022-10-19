b7b778205888dde2ba26a9f4dd3328e1535e2568
andrey <adolgopolov0@gmail.com>
2022-10-19 16:30:36 +0300

    TBS-641 на странице проекта, если нет никаких сообществ выводить заглушку

--- resources/views/common/project/tariff.blade.php
+++ resources/views/common/project/tariff.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-@if(empty($tariffs))
+@if($ids == 'all' || empty($tariffs))
     <section class="community-tab" data-tab="tariffPage">
         <div class="community-tab__main-header">
             <h2 class="community-tab__main-title">
