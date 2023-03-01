bc0c73732e30f90b4fbee46e27776ca0ef558f29
lv <lozovoyv@gmail.com>
2023-03-01 11:01:32 +0300

    review

--- app/Http/Controllers/APIv3/ApiFeedBackController.php
+++ app/Http/Controllers/APIv3/ApiFeedBackController.php
@@ -6,23 +6,38 @@ use App\Http\ApiRequests\ApiFeedBackRequest;
 use App\Http\ApiResponses\ApiResponse;
 use App\Http\Controllers\Controller;
 use App\Models\Feedback;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
 class ApiFeedBackController extends Controller
 {
-    public function store(ApiFeedBackRequest $request):ApiResponse
+    /**
+     * Add feedback from user.
+     *
+     * TODO swagger annotation
+     *
+     * @param ApiFeedBackRequest $request
+     *
+     * @return ApiResponse
+     */
+    public function store(ApiFeedBackRequest $request): ApiResponse
     {
-        $feed_back = Feedback::create([
-            'user_id' => Auth::user()->id,
+        /** @var User $user */
+        $user = Auth::user();
+
+        $feed_back = Feedback::query()->create([
+            'user_id' => $user->id,
             'email' => $request->input('fb_email'),
-            'phone' => $request->input('fb_phone') ?? null,
-            'name' => $request->input('fb_name') ?? null,
+            'phone' => $request->input('fb_phone'),
+            'name' => $request->input('fb_name'),
             'text' => $request->input('fb_message'),
             'status' => 'Новый',
         ]);
-        if(empty($feed_back)){
+
+        if ($feed_back === null) {
             return ApiResponse::error('common.feed_back_error');
         }
-        return ApiResponse::common(['feed_back'=>$feed_back]);
+
+        return ApiResponse::success();
     }
 }
