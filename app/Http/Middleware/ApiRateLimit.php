8714b7b868d4b6bb70b137d6bc24659cf6f1403d
apboro <borodachev@gmail.com>
2023-04-11 10:36:41 +0300

    TBS-978 manual throttle

---
+++ app/Http/Middleware/ApiRateLimit.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Routing\Middleware\ThrottleRequests;
+
+class ApiRateLimit extends ThrottleRequests
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  int|string  $maxAttempts
+     * @param  float|int  $decayMinutes
+     * @param  string  $prefix
+     * @return \Symfony\Component\HttpFoundation\Response
+     *
+     * @throws \Illuminate\Http\Exceptions\ThrottleRequestsException
+     */
+    public function handle($request, Closure $next, $maxAttempts = null, $decayMinutes = 1, $prefix = '')
+    {
+        if($maxAttempts === null) {
+            $maxAttempts = env('APP_RATE_LIMIT');
+        }
+        if (is_string($maxAttempts)
+            && func_num_args() === 3
+            && ! is_null($limiter = $this->limiter->limiter($maxAttempts))) {
+            return $this->handleRequestUsingNamedLimiter($request, $next, $maxAttempts, $limiter);
+        }
+
+        return $this->handleRequest(
+            $request,
+            $next,
+            [
+                (object) [
+                    'key' => $prefix.$this->resolveRequestSignature($request),
+                    'maxAttempts' => $this->resolveMaxAttempts($request, $maxAttempts),
+                    'decayMinutes' => $decayMinutes,
+                    'responseCallback' => null,
+                ],
+            ]
+        );
+    }
+}
