25e057e9f4591cfac95f57d21ae6dd893e80518c
eeu <smirnov_e87@mail.ru>
2023-04-11 20:07:02 +0300

    TBS-941 Fix user kick when in black list

--- app/Listeners/NewChatUserBlackListCheck.php
+++ app/Listeners/NewChatUserBlackListCheck.php
@@ -6,6 +6,7 @@ use App\Events\NewChatUserJoin;
 use App\Models\TelegramUserList;
 use App\Repositories\TelegramUserLists\TelegramUserListsRepositry;
 use App\Services\TelegramMainBotService;
+use Illuminate\Support\Facades\Log;
 
 
 class NewChatUserBlackListCheck
@@ -26,18 +27,21 @@ class NewChatUserBlackListCheck
     /**
      * Handle the event.
      *
-     * @param  object  $event
+     * @param object $event
      * @return void
      */
     public function handle(NewChatUserJoin $event)
     {
-        $telegram_list = TelegramUserList::with(['communities'])->
-        whereHas('communities', function ($q) use ($event) {
-            $q->where('community_id', $event->chat_id)->
-            where('type',TelegramUserListsRepositry::TYPE_BLACK_LIST)->
-            where('telegram_id',$event->telegram_user_id);
-        })->first();
-        if($telegram_list !== null){
+        //Log::log('info', 'start kick user ' . $event->chat_id . ' ' . $event->telegram_user_id);
+        $telegram_list = TelegramUserList::with(['communities.connection'])->whereHas('communities.connection',
+            function ($query) use ($event) {
+                $query->where('chat_id', $event->chat_id);
+            })
+            ->where('type', TelegramUserListsRepositry::TYPE_BLACK_LIST)
+            ->where('telegram_id', $event->telegram_user_id)->first();
+        //Log::log('info', 'start kick user ' . $telegram_list->toSql());
+        if ($telegram_list !== null) {
+            Log::log('info', 'kicked user ' . $event->chat_id . ' ' . $event->telegram_user_id);
             $this->telegramMainBotService->kickUser(
                 config('telegram_bot.bot.botName'),
                 $event->telegram_user_id,
