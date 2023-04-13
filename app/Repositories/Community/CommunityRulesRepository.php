70f3f12ed649bd0e1831e248d7fb7023fa0f28aa
apboro <borodachev@gmail.com>
2023-04-13 11:51:13 +0300

    TBS-813 debug

--- app/Repositories/Community/CommunityRulesRepository.php
+++ app/Repositories/Community/CommunityRulesRepository.php
@@ -215,7 +215,7 @@ class CommunityRulesRepository implements CommunityRulesRepositoryContract
 
                         $this->logger->debug('conditionChecker item', [$item]);
 
-                            if ($item[0]->offset != 0 && ($item[0]->type == "url" || $item->type == "text_link")) {
+                            if ($item->offset != 0 && ($item->type == "url" || $item->type == "text_link")) {
                                 return true; //$this->actionRunner($data, $rule->action);
 
                         }
