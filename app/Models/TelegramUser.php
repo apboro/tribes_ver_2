cfc039517e74486fcba65a481e80c908168d6b74
apboro <borodachev@gmail.com>
2023-04-21 14:24:51 +0300

    fix userList

--- app/Models/TelegramUser.php
+++ app/Models/TelegramUser.php
@@ -117,7 +117,9 @@ class TelegramUser extends Model
             TelegramUserList::class,
             'telegram_id',
             'telegram_id'
-        );
+        )->whereHas('communities', function($query){
+            $query->where('is_active', 1);
+        });
 //            ->leftJoin('communities',
 //            function($join) {
 //                $join->on('communities.id', '=', 'telegram_user_lists.community_id')
