76dd42c027709c900ff0c9f98e2676e82a8acf1e
apboro <borodachev@gmail.com>
2023-04-25 15:26:29 +0300

    ranks is an array of objects

--- app/Http/ApiRequests/UserRank/ApiRankRuleStoreRequest.php
+++ app/Http/ApiRequests/UserRank/ApiRankRuleStoreRequest.php
@@ -10,27 +10,22 @@ use App\Http\ApiRequests\ApiRequest;
  * @OA\Post(
  *  path="/api/v3/chats/rank",
  *  operationId="chats-rank-rule-add",
- *  summary="Add rank rule",
+ *  summary="Add rank rule with ranks",
  *  security={{"sanctum": {} }},
  *  tags={"Chats Rank Rules"},
  *     @OA\RequestBody(
- *         description="
- *          rank_change_in_chat - enum from [вкл,выкл]
- *          first_rank_in_chat - enum from [вкл,выкл]",
- *         @OA\MediaType(
- *             mediaType="application/json",
- *             @OA\Schema(
- *                 @OA\Property(property="rule_name",type="string"),
- *                 @OA\Property(property="rank_names",type="array", @OA\Items(type="string")),
- *                 @OA\Property(property="reputation_value_to_achieve",type="integer"),
- *
+ *         @OA\JsonContent(
+ *                 @OA\Property(property="rule_name", type="string"),
+ *                 @OA\Property(property="ranks",type="array", @OA\Items(
+ *                        @OA\Property(property="name",type="string"),
+ *                        @OA\Property(property="reputation_value_to_achieve",type="integer")
+ *                 )),
  *                 @OA\Property(property="period_until_reset",type="string"),
  *                 @OA\Property(property="rank_change_in_chat",type="boolean"),
  *                 @OA\Property(property="rank_change_message",type="string"),
  *                 @OA\Property(property="first_rank_in_chat",type="boolean"),
  *                 @OA\Property(property="first_rank_message",type="string"),
  *                 ),
- *             ),
  *         ),
  *   @OA\Response(response=200, description="OK")
  *)
@@ -41,8 +36,7 @@ class ApiRankRuleStoreRequest extends ApiRequest
     {
         return [
             'rule_name'                   => ['required', 'string', 'max:120'],
-            'rank_names'                  => ['required', 'array'],
-            'reputation_value_to_achieve' => ['required', 'integer'],
+            'ranks'                       => ['required', 'array'],
             'period_until_reset'          => ['required', 'string'],
             'rank_change_in_chat'         => ['required','boolean'],
             'rank_change_message'         => ['nullable'],
@@ -51,18 +45,4 @@ class ApiRankRuleStoreRequest extends ApiRequest
         ];
     }
 
-    public function prepareForValidation(): void
-    {
-        $this->merge(['rank_change_in_chat' => $this->toBoolean($this->rank_change_in_chat)]);
-        $this->merge(['first_rank_in_chat' => $this->toBoolean($this->first_rank_message)]);
-    }
-
-    /**
-     * Convert to boolean *
-     * @param $booleable * @return boolean
-     */
-    private function toBoolean($booleable)
-    {
-        return filter_var($booleable, FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE);
-    }
 }
