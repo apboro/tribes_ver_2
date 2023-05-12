3fe9bd9cb2f6f7e71dced63c3af227c2dee8a375
apboro <borodachev@gmail.com>
2023-05-12 12:20:05 +0300

    fix multi greetings

--- app/Listeners/NewChatUserBlackListCheck.php
+++ app/Listeners/NewChatUserBlackListCheck.php
@@ -32,21 +32,25 @@ class NewChatUserBlackListCheck
      */
     public function handle(NewChatUserJoin $event)
     {
-        //Log::log('info', 'start kick user ' . $event->chat_id . ' ' . $event->telegram_user_id);
-        $telegram_list = TelegramUserList::with(['communities.connection'])->whereHas('communities.connection',
-            function ($query) use ($event) {
-                $query->where('chat_id', $event->chat_id);
-            })
-            ->where('type', TelegramUserListsRepositry::TYPE_BLACK_LIST)
-            ->where('telegram_id', $event->telegram_user_id)->first();
-        //Log::log('info', 'start kick user ' . $telegram_list->toSql());
-        if ($telegram_list !== null) {
-            Log::log('info', 'kicked user ' . $event->chat_id . ' ' . $event->telegram_user_id);
-            $this->telegramMainBotService->kickUser(
-                config('telegram_bot.bot.botName'),
-                $event->telegram_user_id,
-                $event->chat_id
-            );
+
+        try {
+            //Log::log('info', 'start kick user ' . $event->chat_id . ' ' . $event->telegram_user_id);
+            $telegram_list = TelegramUserList::with(['communities.connection'])->whereHas('communities.connection',
+                function ($query) use ($event) {
+                    $query->where('chat_id', $event->chat_id);
+                })
+                ->where('type', TelegramUserListsRepositry::TYPE_BAN_LIST)
+                ->where('telegram_id', $event->telegram_user_id)->first();//Log::log('info', 'start kick user ' . $telegram_list->toSql());
+            if ($telegram_list !== null) {
+                Log::log('info', 'kicked user ' . $event->chat_id . ' ' . $event->telegram_user_id);
+                $this->telegramMainBotService->kickUser(
+                    config('telegram_bot.bot.botName'),
+                    $event->telegram_user_id,
+                    $event->chat_id
+                );
+            }
+        } catch (\Exception $e) {
+            Log::error($e);
         }
     }
 }
