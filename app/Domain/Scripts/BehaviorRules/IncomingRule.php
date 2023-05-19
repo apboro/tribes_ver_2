101dd5bab5f310d113a7aa676b06433db8b9c089
YahinAF <amir.yahin@fabit.ru>
2023-05-19 18:22:30 +0300

    TBS-1171 fix url build

--- app/Domain/Scripts/BehaviorRules/IncomingRule.php
+++ app/Domain/Scripts/BehaviorRules/IncomingRule.php
@@ -27,7 +27,7 @@ final class IncomingRule
         $chatId = $telegramConnections->chat_id;
         $content = $communityRule->content;
 
-        $imageUrl = env('APP_URL') . $communityRule->content_image_path;
+        $imageUrl = env('APP_URL') . '/'. $communityRule->content_image_path;
         log::info('image url:'. $imageUrl);
 
         /** @var stdClass $response */
