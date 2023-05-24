b6f8bee8a44a8a885c2f7ed9625cf151d77220db
apboro <borodachev@gmail.com>
2023-05-24 11:15:32 +0300

    TBS-1051 request

--- app/Http/ApiRequests/Reputation/ApiCommunityReputationRuleStoreRequest.php
+++ app/Http/ApiRequests/Reputation/ApiCommunityReputationRuleStoreRequest.php
@@ -15,16 +15,26 @@ use App\Http\ApiRequests\ApiRequest;
  *         description="
  *          who_can_rate - enum from [all, owner, owner_and_admin]
  *          show_rating_tables_period - enum from [first_day_of_year, first_day_of_month, first_day_of_week]
- *          restrict_accumulate_rate_period - enum from [first_day_of_year, first_day_of_month, first_day_of_week]
- *          ",
+ *          restrict_accumulate_rate_period - enum from [first_day_of_year, first_day_of_month, first_day_of_week]",
  *         @OA\MediaType(
- *             mediaType="application/json",
+ *             mediaType="multipart/form-data",
+ *             encoding={
+ *                  "community_ids[]": {
+ *                      "explode": true,
+ *                  },
+ *                  "keyword_rate_up[]":{
+ *                      "explode": true,
+ *                  },
+ *                "keyword_rate_down[]":{
+ *                      "explode": true,
+ *                  }
+ *              },
  *             @OA\Schema(
  *                 @OA\Property(property="title",type="string"),
  *                 @OA\Property(property="who_can_rate",type="string",example="all"),
  *                 @OA\Property(property="restrict_rate_member_period",type="integer"),
- *                 @OA\Property(property="delay_start_rules_time",type="string"),
- *                 @OA\Property(property="delay_start_rules_messages",type="integer"),
+ *                 @OA\Property(property="delay_start_rules_seconds",type="string"),
+ *                 @OA\Property(property="delay_start_rules_messages",type="string"),
  *
  *                 @OA\Property(property="show_rating_tables",type="boolean"),
  *                 @OA\Property(property="show_rating_tables_period",type="string"),
@@ -38,11 +48,13 @@ use App\Http\ApiRequests\ApiRequest;
  *                 @OA\Property(property="notify_about_rate_change_image",type="string"),
  *                 @OA\Property(property="notify_about_rate_change_message",type="string"),
  *
- *                 @OA\Property(property="restrict_accumulate_rate",type="string"),
+ *                 @OA\Property(property="restrict_accumulate_rate",type="boolean"),
  *                 @OA\Property(property="restrict_accumulate_rate_period",type="string"),
  *                 @OA\Property(property="restrict_accumulate_rate_image",type="string"),
  *                 @OA\Property(property="restrict_accumulate_rate_message",type="string"),
- *                 @OA\Property(property="community_ids",type="array",@OA\Items(type="integer")),
+ *                 @OA\Property(property="community_ids[]",type="array",@OA\Items(type="integer")),
+ *                 @OA\Property(property="keyword_rate_up[]",type="array",@OA\Items(type="string")),
+ *                 @OA\Property(property="keyword_rate_down[]",type="array",@OA\Items(type="string")),
  *                 ),
  *             ),
  *     ),
@@ -55,35 +67,64 @@ class ApiCommunityReputationRuleStoreRequest extends ApiRequest
     public function rules(): array
     {
         return [
-
-            'ranks_points_to_upgrade' =>'integer',
-            'ranks_names' => 'array',
-
-            'keyword_rate_up' => 'required|array',
-            'keyword_rate_down' => 'required|array',
+            'keyword_rate_up' => 'array',
+            'keyword_rate_down' => 'array',
 
             'title' => 'required|string|max:120',
             'who_can_rate' => 'string',
             'restrict_rate_member_period' => 'string',
+
             'delay_start_rules_time' => 'string',
             'delay_start_rules_total_messages' => 'integer',
+
             'show_rating_tables' => 'bool',
             'show_rating_tables_period' => 'string',
             'show_rating_tables_time' => 'string',
             'show_rating_tables_number_of_users' => 'integer',
             'show_rating_tables_image' => 'image|nullable',
             'show_rating_tables_message' => 'string',
+
             'notify_about_rate_change' => 'bool',
             'notify_about_rate_change_points' => 'integer',
             'notify_about_rate_change_image' => 'image|nullable',
             'notify_about_rate_change_message' => 'string',
+
             'restrict_accumulate_rate' => 'bool',
             'restrict_accumulate_rate_period' => 'string',
             'restrict_accumulate_rate_image' => 'image|nullable',
             'restrict_accumulate_rate_message' => 'string',
+
             'community_ids' => 'array',
             'community_ids.*' => 'integer',
         ];
     }
+
+
+    public function prepareForValidation(): void
+    {
+
+        $this->merge([
+            'show_rating_tables' => $this->toBoolean($this->show_rating_tables)
+        ]);
+        $this->merge([
+            'notify_about_rate_change' => $this->toBoolean($this->notify_about_rate_change)
+        ]);
+        $this->merge([
+            'restrict_accumulate_rate' => $this->toBoolean($this->restrict_accumulate_rate)
+        ]);
+    }
+
+
+    /**
+     * Convert to boolean
+     *
+     * @param $booleable
+     * @return boolean
+     */
+    private function toBoolean($booleable)
+    {
+        return filter_var($booleable, FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE);
+    }
+
 
 }
