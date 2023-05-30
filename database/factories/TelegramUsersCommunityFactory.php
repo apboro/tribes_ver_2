f0fdc5dfa5066f70daaf50eee476f8102bdb81cc
eeu <smirnov_e87@mail.ru>
2023-05-30 11:19:45 +0300

    TBS 1149 Add message statistic

--- database/factories/TelegramUsersCommunityFactory.php
+++ database/factories/TelegramUsersCommunityFactory.php
@@ -4,8 +4,8 @@ namespace Database\Factories;
 
 use App\Models\Community;
 use App\Models\TelegramUser;
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 class TelegramUsersCommunityFactory extends Factory
 {
@@ -16,13 +16,14 @@ class TelegramUsersCommunityFactory extends Factory
      */
     public function definition()
     {
+        $faker = \Faker\Factory::create();
         return [
             'community_id' => Community::all()->random(),
             'telegram_user_id' => TelegramUser::all()->random(),
             'excluded' => '',
             'role' => 'participant',
             'accession_date' => Carbon::now()->timestamp,
-            'exit_date' => null,
+            'exit_date' => $faker->boolean() ? $faker->dateTimeBetween(Carbon::now()->subMonth(), Carbon::now()) : null,
         ];
     }
 }
