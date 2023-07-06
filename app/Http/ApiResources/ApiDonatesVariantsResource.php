b48e3c0f5c2e2adc7945a53fb498ae6524127463
apboro <borodachev@gmail.com>
2023-07-06 15:49:15 +0300

    hotfix donates resource

--- app/Http/ApiResources/ApiDonatesVariantsResource.php
+++ app/Http/ApiResources/ApiDonatesVariantsResource.php
@@ -15,26 +15,17 @@ class ApiDonatesVariantsResource extends JsonResource
     public function toArray($request)
     {
 
-        $data = [
+        return [
             "id" => $this->resource->id,
             "donate_id" => $this->resource->donate_id,
             $this->resource->variant_name . '_is_active' => $this->resource->isActive,
             $this->resource->variant_name . '_button' => $this->resource->description,
             "created_at" => $this->resource->created_at,
             "variant_name" => $this->resource->variant_name,
+            'min_price' => $this->resource->min_price,
+            "max_price" => $this->resource->max_price,
+            $this->resource->variant_name => $this->resource->price,
         ];
 
-        if (!is_null($this->resource->min_price)) {
-            $data["min_price"] = $this->resource->min_price;
-        }
-
-        if (!is_null($this->resource->max_price)) {
-            $data["max_price"] = $this->resource->max_price;
-        }
-
-        if (!is_null($this->resource->price)) {
-            $data[$this->resource->variant_name] = $this->resource->price;
-        }
-        return $data;
     }
 }
