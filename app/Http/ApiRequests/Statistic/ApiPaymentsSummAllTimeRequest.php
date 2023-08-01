3c2e4e46d204690cfd9d5a921b527c22aa266254
boris <boris@magic-present.su>
2023-08-01 11:35:45 +0300

    tbs-1442-add-swagger

---
+++ app/Http/ApiRequests/Statistic/ApiPaymentsSummAllTimeRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\ApiRequests\Statistic;
+
+use App\Http\ApiRequests\ApiRequest;
+
+/**
+ * @OA\Patch(
+ *  path="/api/v3/statistic/payments-all-time",
+ *  operationId="statistic-payments-all-time",
+ *  summary="Finance statistic: tariff, donate, course",
+ *  security={{"sanctum": {} }},
+ *  tags={"Statistic Finance"},
+ *  @OA\Response(response=200, description="OK"),
+ *  @OA\Response(response=419, description="Token mismatch", @OA\JsonContent(ref="#/components/schemas/api_response_token_mismatch")),
+ *)
+ */
+class ApiPaymentsSummAllTimeRequest extends ApiRequest
+{
+
+
+
+}
