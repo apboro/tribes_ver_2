30eb0e986d10a507be1707fe4fdf65420052ed84
boris <boris@magic-present.su>
2023-10-06 14:59:19 +0300

    TBS-1742 add send mail at info@spodial.com

--- app/Listeners/FeedBackListener.php
+++ app/Listeners/FeedBackListener.php
@@ -15,12 +15,18 @@ class FeedBackListener
      */
     public function handle(FeedBackCreate $event): void
     {
+        $vSpodial = view('mail.feedback_spodial_create')->with(
+            [
+                'feedback' => $event->feedback,
+            ]
+        )->render();        
+        new Mailer('Сервис ' . env('APP_NAME'), $vSpodial, 'Вопрос клиента - ' . $event->feedback->id, 'info@spodial.com');
+
         $v = view('mail.feedback_create')->with(
             [
                 'message' => $event->feedback->text,
             ]
         )->render();
-
         new Mailer('Сервис ' . env('APP_NAME'), $v, 'Вы задали вопрос ', $event->user->email);
     }
 }
