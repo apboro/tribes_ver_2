9b35938184c7f47d1f547bedd3aa8bde3a903bbc
boris <boris@magic-present.su>
2023-10-06 14:19:17 +0300

    TBS-1744 add tg info in xlsx for admins

--- app/Http/ApiResources/Admin/UserForManagerResource.php
+++ app/Http/ApiResources/Admin/UserForManagerResource.php
@@ -10,9 +10,27 @@ class UserForManagerResource extends JsonResource
     /** @var User */
     public $resource;
 
+    /**
+     * Возвращает массив с записями Фамилия + Имя пользователя в телеге
+     */
+    private function findFirstLastName()
+    {
+        $firstNames = $this->resource->telegramMeta->pluck('first_name')->toArray() ?? null;
+        $lastNames = $this->resource->telegramMeta->pluck('last_name')->toArray() ?? null;
+        if (!$firstNames) {
+            return null;
+        }
+
+        $result = [];
+        foreach ($firstNames as $key=>$name){ 
+            $result[] = $lastNames[$key] . ' ' . $name;
+        }
+
+    return $result;
+    }
+
     public function toArray($request)
     {
-
         return [
             'id' => $this->resource->id,
             'name' => $this->resource->name,
@@ -20,7 +38,11 @@ class UserForManagerResource extends JsonResource
             'phone' => $this->resource->phone,
             'community_owner_num' => count($this->resource->communities),
             'phone_confirmed' => $this->resource->phone_confirmed,
-            'telegram_user_name' => $this->resource->telegramMeta->pluck('user_name'), //->user_name ?? '—',
+            'telegram_user_name' => array_map(
+                                        fn($value) => '@' . $value,
+                                        $this->resource->telegramMeta->pluck('user_name')->toArray()
+                                    ),
+            'telegram_first_last_name' => $this->findFirstLastName(), 
             'is_blocked' => $this->resource->is_blocked,
             'locale' => $this->resource->locale,
             'role_index' => $this->resource->role_index,
@@ -31,6 +53,7 @@ class UserForManagerResource extends JsonResource
                     ->sum('amount') / 100 ?? '—',
             'created_at' => $this->resource->created_at,
             'updated_at' => $this->resource->updated_at,
+            'communities' => implode(', ', $this->resource->communities->pluck('title')->toArray()),
         ];
     }
 }
