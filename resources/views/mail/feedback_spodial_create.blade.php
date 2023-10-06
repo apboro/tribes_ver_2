313b9b7b03a1e7269f2176f6733ef9b2559ea259
boris <boris@magic-present.su>
2023-10-06 15:12:14 +0300

    TBS-1742 add send mail at info@spodial.com

--- resources/views/mail/feedback_spodial_create.blade.php
+++ resources/views/mail/feedback_spodial_create.blade.php
@@ -57,75 +57,58 @@
                         <tr>
                             <td align="center" class="sm-px-24"
                                 style="font-family: 'Montserrat', sans-serif; mso-line-height-rule: exactly;">
+                                {{ env('APP_NAME') }}
                                 <table style="width: 100%; margin: 0; margin-top: 40px" cellpadding="0" cellspacing="0"
-                                    role="presentation">
-                                    {{ env('APP_NAME') }}
+                                    role="presentation">                                    
                                     <tr>
                                         <td class="sm-px-24"
                                             style="mso-line-height-rule: exactly; border-radius: 4px; background-color: #ffffff; padding: 48px; text-align: left; font-family: Montserrat, -apple-system, 'Segoe UI', sans-serif; font-size: 16px; line-height: 24px; color: #626262;">
 
 
-
-                                            <p
-                                                style="font-family: 'Montserrat', sans-serif; mso-line-height-rule: exactly; margin-top: 0; font-size: 24px; font-weight: 700; color: #ff5850;">
+                                            <p>
                                                 ID обращения:
                                             </p>
-                                            <p
-                                                style="font-family: 'Montserrat', sans-serif; mso-line-height-rule: exactly; margin: 0; margin-bottom: 24px;">
+                                            <p>
                                                 {{$feedback->id}}
                                             </p>
 
-                                            <p
-                                                style="font-family: 'Montserrat', sans-serif; mso-line-height-rule: exactly; margin-top: 0; font-size: 24px; font-weight: 700; color: #ff5850;">
+                                            <p>
                                                 Имя участника:
                                             </p>
-                                            <p
-                                                style="font-family: 'Montserrat', sans-serif; mso-line-height-rule: exactly; margin: 0; margin-bottom: 24px;">
+                                            <p>
                                                 {{$feedback->name}}
                                             </p>
 
-                                            <p
-                                                style="font-family: 'Montserrat', sans-serif; mso-line-height-rule: exactly; margin-top: 0; font-size: 24px; font-weight: 700; color: #ff5850;">
+                                            <p>
                                                 Почта участника:
                                             </p>
-                                            <p
-                                                style="font-family: 'Montserrat', sans-serif; mso-line-height-rule: exactly; margin: 0; margin-bottom: 24px;">
+                                            <p>
                                                 {{$feedback->email}}
                                             </p>
 
-                                            <p
-                                                style="font-family: 'Montserrat', sans-serif; mso-line-height-rule: exactly; margin-top: 0; font-size: 24px; font-weight: 700; color: #ff5850;">
+                                            <p>
                                                 Телефон участника:
                                             </p>
-                                            <p
-                                                style="font-family: 'Montserrat', sans-serif; mso-line-height-rule: exactly; margin: 0; margin-bottom: 24px;">
+                                            <p>
                                                 {{$feedback->phone}}
                                             </p>
 
-                                            <p
-                                                style="font-family: 'Montserrat', sans-serif; mso-line-height-rule: exactly; margin-top: 0; font-size: 24px; font-weight: 700; color: #ff5850;">
+                                            <p>
                                                 Статус обращения:
                                             </p>
-                                            <p
-                                                style="font-family: 'Montserrat', sans-serif; mso-line-height-rule: exactly; margin: 0; margin-bottom: 24px;">
+                                            <p>
                                                 {{$feedback->status}}
                                             </p>
 
-                                            <p
-                                                style="font-family: 'Montserrat', sans-serif; mso-line-height-rule: exactly; margin-top: 0; font-size: 24px; font-weight: 700; color: #ff5850;">
+                                            <p>
                                                 Ваш вопрос:
                                             </p>
-                                            <p
-                                                style="font-family: 'Montserrat', sans-serif; mso-line-height-rule: exactly; margin: 0; margin-bottom: 24px;">
+                                            <p>
                                                 {{$feedback->text}}
                                             </p>
 
                                         </td>
                                     </tr>
-                                    Если мы не решили Ваш вопрос, отправьте, пожалуйста, повторное обращение или
-                                    свяжитесь с нашими специалистами в Telegram.
-                                    <br>
-                                    <a href="{{env('APP_URL')}}">{{env('APP_NAME')}}</a>
                                 </table>
                             </td>
                         </tr>
