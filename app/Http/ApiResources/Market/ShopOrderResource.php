3ca26a6185fa34d4c1aafd699d2436e460b7c1dc
YahinAF <amir.yahin@fabit.ru>
2024-02-06 18:12:56 +0300

    TBS-2002 get order added delivery info

--- app/Http/ApiResources/Market/ShopOrderResource.php
+++ app/Http/ApiResources/Market/ShopOrderResource.php
@@ -29,7 +29,9 @@ class ShopOrderResource extends JsonResource
             'payments_id'      => $this->resource->payments_id,
             'telegram_user_id' => $this->resource->telegram_user_id,
             'delivery_id'      => $this->resource->delivery_id,
+            'delivery'         => $this->resource->delivery,
             'shop_id'          => $this->resource->shop_id,
+            'created_at'       => $this->resource->created_at,
         ];
     }
 }
